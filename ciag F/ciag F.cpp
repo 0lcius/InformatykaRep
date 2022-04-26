da56616914da70c33139b8bfd4b28e8b085901ce
0lcius <olciapro@wp.pl>
2022-04-26 11:53:51 +0200

    Ciag Fibonacciego

---
+++ ciag F/ciag F.cpp
@@ -0,0 +1,35 @@
+﻿
+#include <iostream>
+using namespace std;
+
+void Fib(int F);
+int main()
+{
+	int N;
+	cout << "Podaj N";
+	cin >> N;
+	Fib(N);
+}
+
+void Fib(int F)
+{
+	int f0 = 0;
+	int f1 = 1;
+	int f;
+	
+	for (int i = 0; i < F; i++) {
+		if (i > 1) {
+			f = f0 + f1;
+			f0 = f1;
+			f1 = f;
+
+
+		}
+		else f = i;
+		cout << f;
+		cout << " ";
+	}
+
+
+
+}
